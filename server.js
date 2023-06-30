268173e0c3ad4592e4188b2ef942739782fd1257
David Dubbeldam <khuchongnhunghva@gmail.com>
2023-07-01 01:00:21 +0700

    use npm

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Test")
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log('run request..')
+    res.setHeader('Conten-Type', 'text/html');
+    res.write('<h1>Hello world</h1>')
+    res.end
+})
+
+server.listen(3002, 'localhost', () => {
+    console.log('Node.JS server is running on port: 3002')
+})
