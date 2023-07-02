0f0eda1a12d6ab13e6cfcc1762962f8b3a6098e0
David Dubbeldam <khuchongnhunghva@gmail.com>
2023-07-03 05:27:22 +0700

    video8

---
+++ src/route/web.js
@@ -0,0 +1,14 @@
+import express from "express";
+import homeController from '../controller/homeController';
+let router = express.Router();
+
+const initWebRoute = (app) => {
+    // https://expressjs.com/en/4x/api.html#router giải thích cho đoạn bên dưới
+    router.get('/', homeController.getHomepage);
+
+    router.get('/about', homeController.getHomepage);
+
+    return app.use('/', router)
+}
+
+module.exports = initWebRoute;
--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
-
+import initWebRoute from './route/web'
 require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT || 3000;
 console.log('check port: ', port)
 
+//setup view engine
 configViewEngine(app);
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-})
-app.get('/about', (req, res) => {
-    res.send("tui là Nhun");
-})
+//init webroute
+initWebRoute(app);
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
